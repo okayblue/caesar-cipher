43fdf0b2fb27435081d229537556d4a6f2ecae81
okayblue <56899411+okayblue@users.noreply.github.com>
2022-06-18 16:09:39 -0400

    Add alphabet arrays, split string into an array

--- caesarcipher.rb
+++ caesarcipher.rb
@@ -1,5 +1,10 @@
 def caesar_cipher(string, shift_factor)
-   
+  # create arrays for uppercase and lowercase letters
+  capital_letters = ('A'..'Z').to_a
+  lowercase_letters = ('a'..'z').to_a
+  # Split the string to work with the letters individually
+  split_string = string.split("")  
+
 end
 
 # for testing - this should return "Bmfy f xywnsl!"
