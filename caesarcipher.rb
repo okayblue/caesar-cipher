1e82ccedacf5c593ae45c785f357216c78626e3d
okayblue <56899411+okayblue@users.noreply.github.com>
2022-06-18 20:17:05 -0400

    Add code for shifting the letters

--- caesarcipher.rb
+++ caesarcipher.rb
@@ -2,10 +2,23 @@ def caesar_cipher(string, shift_factor)
   # create arrays for uppercase and lowercase letters
   capital_letters = ('A'..'Z').to_a
   lowercase_letters = ('a'..'z').to_a
-  # Split the string to work with the letters individually
-  split_string = string.split("")  
+  # split the string to work with the letters individually
+  split_string = string.split("")
+  
+  shift = split_string.map do |char|
+    # find the index of the current char in letters array and add the shift_factor
+    if capital_letters.include?(char)
+      char = capital_letters[capital_letters.find_index(char) + shift_factor]
+    elsif lowercase_letters.include?(char)
+      char = lowercase_letters[lowercase_letters.find_index(char) + shift_factor]
+    else
+      # don't change if it isn't a letter
+      char
+    end
+  end
 
+  shift.join
 end
 
 # for testing - this should return "Bmfy f xywnsl!"
-caesar_cipher("What a string!", 5)
+p caesar_cipher("What a string!", 5)
