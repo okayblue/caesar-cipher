c15e83a5c91160aeb77fe364200fb6f21d83a770
okayblue <56899411+okayblue@users.noreply.github.com>
2022-06-22 18:03:02 -0400

    Wrap the letters arrays back to beginning with modulo operator

--- caesarcipher.rb
+++ caesarcipher.rb
@@ -8,17 +8,17 @@ def caesar_cipher(string, shift_factor)
   shift = split_string.map do |char|
     # find the index of the current char in letters array and add the shift_factor
     if capital_letters.include?(char)
-      char = capital_letters[capital_letters.find_index(char) + shift_factor]
+      char = capital_letters[(capital_letters.find_index(char) + shift_factor) % 26] 
     elsif lowercase_letters.include?(char)
-      char = lowercase_letters[lowercase_letters.find_index(char) + shift_factor]
+      char = lowercase_letters[(lowercase_letters.find_index(char) + shift_factor) % 26]
     else
       # don't change if it isn't a letter
       char
     end
   end
 
-  shift.join
-end
+  shift.join("")
+end 
 
 # for testing - this should return "Bmfy f xywnsl!"
 p caesar_cipher("What a string!", 5)
